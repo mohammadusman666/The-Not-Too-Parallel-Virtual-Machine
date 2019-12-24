a28730b6ed46eee9ff474f15a591bab18e7e592c
mohammadusman666 <mohammadusman666@gmail.com>
2019-12-24 22:49:16 +0500

    Broadcast, Terminate Packet & CtrlC signal handled

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
+#define _XOPEN_SOURCE 700
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
 #include <string.h>
 #include <errno.h>
 #include <sys/types.h>
@@ -10,6 +13,8 @@
 
 #define MAX_LINE_SIZE 100
 #define TERMINATE_STRING "$\n"
+
+static volatile sig_atomic_t doneFlag = 0;
 
 static char *typename[] = {"Start Task", "Data", "Broadcast", "Done", "Terminate", "Barrier"};
 
@@ -21,15 +26,30 @@ void *outputFunc(void *param);
 task_array tasks; // tasks array
 pthread_mutex_t lock; // thread mutex lock
 
+static void intHandler(int signo)
+{
+    doneFlag = 1;
+}
+
 int main(void)
 {
     int i; // iterator
     pthread_t input_thread;
     int threadCreationResult;
+    struct sigaction act;
 
     // initialize tasks array object
     memset(tasks.tasks, 0, sizeof(tasks.tasks));
     tasks.numOfTasks = 0;
+
+    // Ctrl-C handler
+    act.sa_handler = intHandler;
+    act.sa_flags = 0;
+    if ((sigemptyset(&act.sa_mask) == -1) || (sigaction(SIGINT, &act, NULL) == -1))
+    {
+        fprintf(stderr, "Failed to install SIGINT signal handler\n");
+        return -1;
+    }
 
     // create the mutex lock
     // 2nd arg NULL -> default attributes
@@ -110,7 +130,7 @@ int getpacket(int fd, int *compidp, int *taskidp, packet_t *typep, int *lenp, un
     }
     fgetc(fptr); // consume \n
 
-    if ((*typep != 0) && (*typep != 1) && *typep != 3)
+    if ((*typep < 0) && (*typep > 5))
     {
         fprintf(stderr, "Got invalid packet! (%d)\n", *typep);
         return 1;
@@ -244,13 +264,11 @@ void *inputFunc(void *param)
     tin = STDIN_FILENO;
     tout = STDOUT_FILENO;
 
-    while (((result = getpacket(tin, &compid, &taskid, &type, &tdatalen, buf)) != -1) && (errno != EINVAL))
+    while (((result = getpacket(tin, &compid, &taskid, &type, &tdatalen, buf)) != -1) && (errno != EINVAL) && (!doneFlag))
     {
         fprintf(stderr, "Get Packet: %d %d %d %d %s\n", compid, taskid, type, tdatalen, buf);
 
-        // 1 is returned by getpacket when BROADCAST, BARRIER or TERMINATE are received
-        /* 1. Output an error message.
-        2. Discard the packet. */
+        // 1 is returned if packet if of invalid type
         if (result == 1)
         {
             continue;
@@ -366,15 +384,69 @@ void *inputFunc(void *param)
             {
                 /* copy the data portion to writefd */
                 // if ((error = r_write(mypipefd[1], buf, tdatalen)) == -1)
-                if ((error = r_write(writefdarray[result], buf, tdatalen)) == -1)
+                // if ((error = r_write(writefdarray[result], buf, tdatalen)) == -1)
+                // {
+                //     fprintf(stderr, "%s", strerror(errno));
+                //     break; // break the loop and exit
+                // }
+                if (putpacket(writefdarray[result], compid, taskid, type, tdatalen, buf) == -1) // 4th argument (1 = DATA)
                 {
-                    fprintf(stderr, "%s", strerror(errno));
-                    break; // break the loop and exit
+                    continue;
                 }
 
                 /* Update the recvpackets and recvbytes members of the appropriate task entry of the tasks object */
                 tasksarray->tasks[result]->recvpackets += 1;
                 tasksarray->tasks[result]->recvbytes += tdatalen;
+            }
+        }
+        /* 2 => BROADCAST
+        When the dispatcher receives a BROADCAST request from standard input, it forwards the packet
+        on the writefd descriptors for each task whose computation ID matches that of the BROADCAST
+        packet. If the dispatcher receives a BROADCAST request from one of the readfd descriptors, it
+        forwards the packet on the writefd descriptors for each task whose computation ID matches
+        that in the BROADCAST packet. Since, in a future extension, tasks from the computation may
+        reside on other hosts, the dispatcher also forwards the packet on its standard output.
+        */
+        else if (type == 2)
+        {
+            /* check the task in tasks array */
+            // result will have the index if found in tasks array
+            result = checkTask(tasksarray, compid, taskid);
+            if (result == -1)
+            {
+                fprintf(stderr, "Computation Id and Task Id don't match!\n");
+                continue;
+            }
+            else
+            {
+                int i;
+                ntpvm_task_t *task;
+
+                for (i = 0; i < MAX_TASKS; i++)
+                {
+                    task = tasksarray->tasks[i];
+
+                    if (task)
+                    {
+                        // if compid matches
+                        if ((task->compid == compid) && (tasksarray->tasks[result]->taskid) != taskid)
+                        {
+                            if (putpacket(writefdarray[i], compid, taskid, type, tdatalen, buf) == -1)
+                            {
+                                continue;
+                            }
+                            /* Update the recvpackets and recvbytes members of the appropriate task entry of the tasks object */
+                            tasksarray->tasks[i]->recvpackets += 1;
+                            tasksarray->tasks[i]->recvbytes += tdatalen;
+                        }
+                    }
+                }
+
+                // the dispatcher also forwards the broadcast packet on its standard output
+                if (putpacket(tout, compid, taskid, type, tdatalen, buf) == -1)
+                {
+                    continue;
+                }
             }
         }
         /* 3 => DONE
@@ -401,18 +473,89 @@ void *inputFunc(void *param)
                 tasksarray->tasks[result]->endinput = 1;
             }
         }
+        /* 4 => TERMINATE */
+        else if (type == 4)
+        {
+            /* check the task in tasks array */
+            // result will have the index if found in tasks array
+            result = checkTask(tasksarray, compid, taskid);
+            if (result == -1)
+            {
+                fprintf(stderr, "Computation Id and Task Id don't match!\n");
+                continue;
+            }
+            else
+            {
+                /* close the writefd descriptor if it is still open */
+                while (((error = close(writefdarray[result])) == -1) && (errno == EINTR));
+
+                /* set the endinput member for this task entry */
+                tasksarray->tasks[result]->endinput = 1;
+            }
+        }
+        /* 5 => BARRIER
+        When the dispatcher receives a BARRIER packet from a task, it sets the barrier member for
+        that task to the barrier number specified by the packet data. When all the tasks in a
+        computation have reported that they are waiting for the barrier, the dispatcher sends a
+        BARRIER message on standard output.
+        When the dispatcher reads a BARRIER packet for that barrier number from standard input, it
+        resets the barrier member to –1 and sends a SIGUSR1 signal to all the tasks in the
+        computation. The BARRIER packet from standard input signifies that all tasks in the computation
+        are waiting at the designated barrier and that they can be released. Assume that the dispatcher
+        never receives a second BARRIER packet from standard input before it has forwarded a
+        corresponding BARRIER packet on standard output.
+        Implement the barrier on the task side by blocking the SIGUSR1 signal, writing a BARRIER
+        packet to standard output, and then executing sigsuspend in a loop until the SIGUSR1 signal
+        arrives. */
+        else if (type == 5)
+        {
+            // result = checkTask(tasksarray, compid, taskid); // check the task in tasks array
+            // if (result == -1)
+            // {
+            //     fprintf(stderr, "Computation Id and Task Id don't match or endinput is true!\n");
+            // }
+            // else
+            // {
+            //     int barrierNum;
+
+            //     error = sscanf(buf, "%d", &barrierNum);
+            //     // if no error occurs
+            //     if ((error > 0) && (error != EOF))
+            //     {
+            //         fprintf(stderr, "%s%d\n", "Barrier Number: ", barrierNum);
+
+            //         tasksarray->tasks[result]->barrier = barrierNum;
+            //     }
+            // }
+        }
     }
 
     // close pipe write end
     // result will have the index if found in tasks array
-    result = checkTask(tasksarray, compid, taskid); // check the task in tasks array
-    if (result == -1)
+    // result = checkTask(tasksarray, compid, taskid); // check the task in tasks array
+    // if (result == -1)
+    // {
+    //     fprintf(stderr, "Computation Id and Task Id don't match or endinput is true!\n");
+    // }
+    // else
+    // {
+    //     while (((error = close(writefdarray[result])) == -1) && (errno == EINTR));
+    // }
+
+    ntpvm_task_t *taski;
+    int i;
+
+    fprintf(stderr, "\nProgram terminating ...\n\n");
+
+    for (i = 0; i < MAX_TASKS; i++)
     {
-        fprintf(stderr, "Computation Id and Task Id don't match or endinput is true!\n");
-    }
-    else
-    {
-        while (((error = close(writefdarray[result])) == -1) && (errno == EINTR));
+        taski = tasks.tasks[i];
+
+        if (taski)
+        {
+            while (((error = close(taski->writefd)) == -1) && (errno == EINTR));
+            while (((error = close(taski->readfd)) == -1) && (errno == EINTR));
+        }
     }
 
     pthread_exit(0);
@@ -431,6 +574,8 @@ void *outputFunc(void *param)
     int key, compid, taskid, tdatalen, recvbytes;
     packet_t type;
     int tout, error;
+
+    unsigned char tempBuf[MAX_PACK_SIZE];
     /***** TASK *****/
 
     key = *((int *) param);
@@ -441,6 +586,7 @@ void *outputFunc(void *param)
     recvbytes = task->recvbytes;
     tout = STDOUT_FILENO;
 
+    // 1st time the task is run
     while ((error = r_read(task->readfd, buf, MAX_PACK_SIZE)) != 0)
     {
         pthread_mutex_lock(&lock); // lock thread mutex
@@ -471,7 +617,86 @@ void *outputFunc(void *param)
 
         pthread_mutex_unlock(&lock); // unlock thread mutex
         // task->mlock = NULL; // remove thread mutex
+        break;
     }
+    fprintf(stderr, "%s\n\n", "Out of 1st loop!");
+
+    taskpacket_t pack;
+    int wsize = sizeof(taskpacket_t);
+
+    while ((error = r_read(task->readfd, &pack, wsize)) != 0)
+    {
+        pthread_mutex_lock(&lock); // lock thread mutex
+        task->mlock = lock; // save thread mutex
+
+        fprintf(stderr, "%d %d %d %d\n", pack.compid, pack.taskid, pack.type, pack.length);
+
+        r_read(task->readfd, &tempBuf, pack.length);
+
+        fprintf(stderr, "%s\n", tempBuf);
+
+        /* 1 => DATA */
+        if (pack.type == 1)
+        {
+            if (!pack.length)
+            {
+                continue;
+            }
+
+            if (putpacket(tout, pack.compid, pack.taskid, pack.type, pack.length, tempBuf) == -1) // 4th argument (1 = DATA)
+            {
+                break;
+            }
+
+            recvbytes = task->recvbytes - recvbytes;
+            task->sentbytes += recvbytes;
+            task->sentpackets += 1;
+        }
+        /* 2 => BROADCAST */
+        else if (pack.type == 2)
+        {
+            int i;
+            ntpvm_task_t *taski;
+
+            if (!pack.length)
+            {
+                continue;
+            }
+
+            for (i = 0; i < MAX_TASKS; i++)
+            {
+                taski = tasks.tasks[i];
+
+                if (taski)
+                {
+                    // if compid matches
+                    if (taski->compid == pack.compid)
+                    {
+                        if (putpacket(taski->writefd, pack.compid, pack.taskid, pack.type, pack.length, tempBuf) == -1)
+                        {
+                            continue;
+                        }
+                        /* Update the recvpackets and recvbytes members of the appropriate task entry of the tasks object */
+                        tasks.tasks[i]->recvpackets += 1;
+                        tasks.tasks[i]->recvbytes += tdatalen;
+                    }
+                }
+                // task->sentbytes += recvbytes;
+                // task->sentpackets += 1;
+            }
+        }
+
+        fprintf(stderr, "Sent: %d %d\n", task->sentbytes, task->sentpackets);
+
+        fprintf(stderr, "\nTask Information:\n");
+        fprintf(stderr, "CompID: %d, TaskID: %d\n", compid, taskid);
+        fprintf(stderr, "Bytes Sent: %d, Packets Sent: %d\n", task->sentbytes, task->sentpackets);
+        fprintf(stderr, "Bytes Received: %d, Packets Received: %d\n\n", task->recvbytes, task->recvpackets);
+
+        pthread_mutex_unlock(&lock); // unlock thread mutex
+        // task->mlock = NULL; // remove thread mutex
+    }
+    fprintf(stderr, "%s\n\n", "Out of 2nd loop!");
 
     /* After falling through the loop because of an end-of-file or an error on readfd, the output thread
     does the following */
@@ -486,6 +711,9 @@ void *outputFunc(void *param)
     type = 3; // 3 => DONE
     tdatalen = 0;
     *buf = 0;
+    pthread_mutex_lock(&lock); // lock thread mutex
+    task->mlock = lock; // save thread mutex
+
     if (putpacket(tout, compid, taskid, type, tdatalen, buf) == -1) // 4th argument (1 = DATA)
     {
         return NULL;
@@ -499,6 +727,8 @@ void *outputFunc(void *param)
     fprintf(stderr, "CompID: %d, TaskID: %d\n", compid, taskid);
     fprintf(stderr, "Bytes Sent: %d, Packets Sent: %d\n", task->sentbytes, task->sentpackets);
     fprintf(stderr, "Bytes Received: %d, Packets Received: %d\n\n", task->recvbytes, task->recvpackets);
+
+    pthread_mutex_unlock(&lock); // unlock thread mutex
 
     // 5. Deactivate the task entry by setting the computation ID to –
     // task->compid = '-';
